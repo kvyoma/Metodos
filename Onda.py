7bcd18c9356fa607f8715589befb8068d945687a
kvyoma <kv.herrera10@uniandes.edu.co>
2017-07-20 19:07:28 -0500

    Update Onda.py

--- Onda.py
+++ Onda.py
@@ -39,3 +39,14 @@ u_presente[0] = 0.0;
 u_presente[n_x-1] = 0.0;
 
 u_futuro=np.zeros((n_t, n_x, n_y))
+
+#Primera iteracion 2 for y
+for i in range(1, n_x-1): 
+		u_presente[i] = u_inicial[i] + ((r**2.0)/2.0) * (u_inicial[i+1] - 2.0 * u_inicial[i] + u_inicial[i-1])
+
+
+#Iteraciones 3 for t, x, y 
+for i in range(1, n_x-1):
+    u_futuro[i] = (2.0 * (1.0 - ((r**2.0)/2.0)))*u_presente[i] - u_pasado[i] + (((r**2.0)/2.0))*(u_presente[i+1] + u_presente[i-1])
+    u_pasado=u_presente.copy()
+    u_presente=u_futuro.copy()
